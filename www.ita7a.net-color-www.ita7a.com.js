7e5d78d31bee4576292298d6c1441527a70f8cd1
ita7a <57022144+ita7a@users.noreply.github.com>
2019-11-17 20:07:43 +0300

    Create www.ita7a.net-color-www.ita7a.com.js

---
+++ www.ita7a.net-color-www.ita7a.com.js
@@ -0,0 +1,48 @@
+$('.checkbox label').html(`<input id="stealth" type="checkbox" value=""><img id="as13" src="/sico/1572134689532.png" width="70%" style="">`)
+'use strict';
+var _0x46c7 = [".nav-tabs", "insertBefore", $(`<img src="/sico/1573415771152.png" class="fr" style="width:100%;margin-top: 1px;"> 'use strict';
+<a href="#" style="border-radius: 15px 0px;width:110px;margin:3px;" class="fl mini  fa fa-star-half-o btn btn-default">⠀الإشتراكات⠀</a>
+<a href="#" style="border-radius: 15px 0px;width:110px;margin:3px;" class="fl mini fa fa-bell btn btn-default">⠀القوانيـن⠀</a>  
+<a href="#" style="border-radius: 15px 0px;width:110px;margin:3px;" class="fl mini  fa fa-heartbeat btn btn-default">⠀التطبيـق⠀</a>  
+<div style="border-radius: 2px 9px 9px 9px;width:100%;margin:2px;border: 2px solid #8F0000;"  id="asim33" class="btn btn-primary btn-sm" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample"> ادارة الموقع</div><div class="collapse" id="collapseExample"><center><div class="well"><b>
+<font color="#78939C"> اداره  الموقع </font> </BR>
+</BR>  MuHanad T7 </BR>
+ </BR> للشكاوي والاستفسارات </BR>
+<font color="#BF1D6E">Line:mudx999~Snap:muh.911</font></BR>     
+`).insertBefore('.nav-tabs');$("<a href=' ' target='_blank' class='fr btn btn-info fa fa-heart'>...... iTa7a.......</a>").insertAfter('.loginstat');$(".label-primary").css("border-radius","24px 24px 24px 24px")
+$(`<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">
+<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: 'Changa', sans-serif; height: 20px;color:#FFF7AB;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5"> 🌸 اهلا وسهلا بكم .. في (شات التحليه للجوال) لمعاوده زيارتنا الكتابه في محركات البحث قوقل (شات التحليه) نتمنى لكم قضاء أجمل واسعد الاوقات
+</marquee></font></b></td>
+`).insertBefore('span.s1.fa.fa-user.label.badgex.label-as-badge.label-success');$("div#tlogins button.btn.btn-primary").click(function(){var myVar=setInterval(function(){var usmsgw=$(".pmsgc").length;if(usmsgw>0){$(`<div class="uzr fl corner borderg mm" style="border-radius:5px;margin-bottom:-2px;width:99.5%;padding:0px; background-color:#F7E7E7;">
+<img style="width: 36px; height: 38px; margin-left: 1px; margin-top: 1px; background-image: url;" class="fl fitimg hand u-pic ">
+<div class="uzr fl" style="padding:0px;width:80%">
+<div style="padding:0px;width:100%;" class="fl">
+<img class="fl u-ico" alt="" src="/sico/1569974559772.gif">
+<span style="padding: 1px 8px; margin-top: 2px; display: block; max-width: 82%; border-radius: 3px; color: rgb(199, 103, 48);" class="corner nosel u-topic dots fl hand">‎ WwW.iTa7a.CoM </span>
+</div>
+<br>
+<div style="padding: 0px; width: 100%; color: rgb(60, 0, 255);" class=" u-msg break fl">
+<div style="padding: 0px;width: 100%;color: rgb(60, 0, 255);text-align: left;display: block;margin-top: -17px;" class=" u-msg break fl"> مرحباً بك عزيزي الزآئر 👋 <h1 style="display: inline-block;color: red;">`+getuser(myid).topic+`</h1> نقدّر تواجدك معنا، ونتمنى لك يوماً سعيداً إن شاء الله </div>
+</div>
+</div></div>
+`).appendTo('div#d2')
+clearInterval(myVar);}else{console.log(usmsgw)}},2000);})
+$(`<center><div><div width="99.5%" style="background-image:url(/sico/1573415726638.jpg);color: #000000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ABABAB;border-top: 2px solid #ABABAB;padding-bottom: 2px!important;" ><font style="background-color: #BCC6CF;border-radius: 15px 15px 15px 15px;font-family: 'jazeera',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" >مرحبآ بكم في شات التحليه </font></div></div><center>`).insertBefore('#d2');$('.nav-tabs').addClass('fl').css('width','100%');$
+('a[href="/sico/1572095805304.jpg"]').removeAttr('href');$(`<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"><link rel="stylesheet" href="">
+</style>`).insertBefore('span.fl.fa.fa-send');
+"div#tlogins button.btn.btn-primary", "https://rawcdn.githack.com/waleed-qaid/des/be9e1d25a3f5960e1ab57e2dca7fd843952cdfbe/www.1des.net-chatt-www.s6acl.com-color1.js", 
+"getScript", "width", "100%", "css", "fl", "addClass", "href", "removeAttr", 'a[href="https://jawalhost.com/"]', "body", '<link rel="stylesheet" type="text/css" href="">\r\n\r\n<link href="https://rawcdn.githack.com/waleed-qaid/des/c4032f6c7d1463651276473d50f3de30ff84d176/www.1des.net-color1-www.skr0.com.css" rel="stylesheet" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/des/23cab940cc1033b45688c72e22e3565a7e659063/www.1des.net-color2-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co2" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/des/23cab940cc1033b45688c72e22e3565a7e659063/www.1des.net-color3-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co3" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/des/23cab940cc1033b45688c72e22e3565a7e659063/www.1des.net-color4-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co4" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/des/23cab940cc1033b45688c72e22e3565a7e659063/www.1des.net-color5-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co5" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/des/c272a7d15ea3a4f1a2375250a30bdca80fae8970/www.1des.net-color6-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co6" type="text/css" />\r\n<link href="https://rawcdn.githack.com/waleed-qaid/des/23cab940cc1033b45688c72e22e3565a7e659063/www.1des.net-color7-www.skr0.com.css" media="screen" rel="alternate stylesheet" title="Co7" type="text/css" />\r\n<style>\r\n\r\n.MyColors {\r\n position: absolute;\r\n transform:matrix(1, 0, 0, 1, 79, 5);\r\n\tmargin-top: 2px;\r\n width: 300px;\r\n height: 40px;\r\n float: left;\r\n background: transparent url(https://skr0.com/sico/z1dplbn6u910.png) no-repeat scroll right top;\r\n text-align: left;\r\n}\r\n\r\na.label.label-primary.fl img.fl,button.btn.fr.btn-success.fa.fa-refresh {\r\n display: none\r\n}\r\n\r\n</style>\r\n', 
+"#d2", '<center><div><div width="99.5%" style="background-image:url(https://skr0.com/sico/z1dorkl9fe10.jpg);color: #000000;border-radius: 5px 5px 5px5px;border-bottom: 2px solid #ABABAB;border-top: 2px solid #ABABAB;padding-bottom: 2px!important;" ><font style="background-color: #CCC0D1;border-radius: 15px 15px 15px 15px;font-family: \'jazeera\',FontAwesome;padding: 2px 10px 2px 10px;margin: 7px 10px 7px 10px;" >\u0645\u0631\u062d\u0628\u0622 \u0628\u0643\u0645 \u0641\u064a \u0634\u0622\u062a \u0633\u0643\u0631 </font></div></div><center>', 
+'a[href="https://skr0.com/sico/z1dorklsaa10.jpg"]', "span.fl.fa.fa-send", '<link rel="stylesheet" type="text/css" href="//www.fontstatic.com/f=jazeera-light,jazeera"><link rel="stylesheet" href="assl.css">\r\n</style>', ".loginstat", "insertAfter", "<a href=' ' target='_blank' class='fr btn btn-danger fa fa-star'>...... \u044f\u0454\u0454\u043c\u03b1.......</a>", "span.s1.fa.fa-user.label.badgex.label-as-badge.label-success", '<link href="https://fonts.googleapis.com/css?family=Changa" rel="stylesheet">\r\n<marquee direction="right" width="65%" direction="right" style="margin-bottom: -7px;font-family: \'Changa\', sans-serif; height: 20px;color:#FFF7AB;" onmouseover="this.stop()" onmouseout="this.start()" scrolldelay="0" scrollamount="5">\ud83c\udf38\u0645\u0631\u062d\u0628\u0627 \u0628\u0643\u0645 \u0646\u062a\u0645\u0646\u0649 \u0644\u0643\u0645 \u0642\u0636\u0627\u0621 \u0623\u062c\u0645\u0644 \u0627\u0644\u0627\u0648\u0642\u0627\u062a\r\n</marquee></font></b></td>\r\n'];
+$(_0x46c7[2])[_0x46c7[1]](_0x46c7[0]);
+$(_0x46c7[12])[_0x46c7[11]](function() {
+var _0x19acx1$jscomp$0 = setInterval(function() {
+var _0x19acx2$jscomp$0 = $(_0x46c7[4])[_0x46c7[3]];
+if (_0x19acx2$jscomp$0 > 0) {
+$(_0x46c7[7] + getuser(myid)[_0x46c7[8]] + _0x46c7[9])[_0x46c7[6]](_0x46c7[5]);
+clearInterval(_0x19acx1$jscomp$0);
+} else {
+console[_0x46c7[10]](_0x19acx2$jscomp$0);
+}
+}, 2E3);
+});
